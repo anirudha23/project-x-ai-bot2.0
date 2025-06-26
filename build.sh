a1aa2ce3dd271f0fc59f1feed2359d96fb2e3bc5
anirudha23 <114842691+anirudha23@users.noreply.github.com>
2025-06-26 23:10:50 +0530

    all file working

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Chromium and dependencies
+apt-get update
+apt-get install -y chromium chromium-driver fonts-liberation libappindicator3-1 libasound2 \
+  libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libnss3 \
+  libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 xdg-utils --no-install-recommends
+
+# Install Python dependencies
+pip install -r requirements.txt
